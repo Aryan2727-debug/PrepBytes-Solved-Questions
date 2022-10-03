92f80b3bcecbeff59b370d599e0a541a7ca0dee3
Aryan Dev Shourie <78311368+Aryan2727-debug@users.noreply.github.com>
2022-10-04 00:37:15 +0530

    Add files via upload

--- Height_of_Tree.cpp
+++ Height_of_Tree.cpp
@@ -1,11 +1,21 @@
+// Complete the calculateHeight function below.
+/* For your reference
+struct node
+{
+    long long value;
+    node *left;
+    node *right;
+};
+*/
+
 int calculateHeight(node* node)
 {
     //write your code here
-    if(node==NULL){
+    if(node == NULL){
       return 0;
     }else{
-      int lDepth=calculateHeight(node->left);
-      int rDepth=calculateHeight(node->right);
+      int lDepth = calculateHeight(node->left);
+      int rDepth = calculateHeight(node->right);
       
       if(lDepth>rDepth){
         return (lDepth+1);
